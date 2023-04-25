c9f862cd0cda6074a9a75343a26a3070daa4d8d9
chaonianye <chaonianye@cnpc.com.cn>
2023-04-25 12:07:57 +0800

    图标的使用

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,26 @@
 <template>
     <div class="login_container">
         <div class="login_box">
+            <!-- 头像区域 -->
             <div class="avatar_box">
                 <img src="../assets/logo.png" alt="">
             </div>
+            <!-- 登录表单区域 -->
+            <el-form label-width="0px" class="login_form">
+                <!-- 用户名 -->
+                <el-form-item>
+                    <el-input prefix-icon="el-icon-user"></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                <el-form-item>
+                    <el-input prefix-icon="el-icon-lock"></el-input>
+                </el-form-item>
+                <!-- 按钮区域 -->
+                <el-form-item class="btns">
+                    <el-button type="primary">登录</el-button>
+                    <el-button type="info" >重置</el-button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
@@ -40,8 +57,9 @@ export default {
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%,-50%);
-    .avatar_box{
+    transform: translate(-50%, -50%);
+
+    .avatar_box {
         height: 130px;
         width: 130px;
         border: 1px solid #eee;
@@ -49,14 +67,26 @@ export default {
         padding: 10px;
         position: absolute;
         left: 50%;
-        transform: translate(-50%,-50%);
+        transform: translate(-50%, -50%);
         background-color: #fff;
-        img{
+
+        img {
             height: 100%;
             widows: 100%;
             border-radius: 50%;
             background-color: #eee;
         }
+    }
+    .btns{
+        display: flex;
+        justify-content: flex-end;
+    }
+    .login_form{
+        position: absolute;
+        bottom: 0px;
+        width: 100%;
+        padding: 0 20px;
+        box-sizing: border-box;
     }
 }
 </style>
