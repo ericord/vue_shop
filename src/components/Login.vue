054fa8218ba72e1cdcbd976f34576aaf36204e3a
chaonianye <chaonianye@cnpc.com.cn>
2023-04-25 08:52:42 +0800

    路由组件,路由重定向

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        登录行为
+    </div>
+</template>
+<script >
+export default {
+    // name: 'VueShopLogin',
+
+    // data() {
+    //     return {
+            
+    //     };
+    // },
+
+    // mounted() {
+        
+    // },
+
+    // methods: {
+        
+    // },
+};
+</script >
+<style lang="less" scoped>
+    
+</style>
