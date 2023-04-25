a3cbd4c35adeef2ddbc14cd639cddf315846ce7e
chaonianye <chaonianye@cnpc.com.cn>
2023-04-25 10:41:43 +0800

    头像区域布局

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        登录行为
+    <div class="login_container">
+        <div class="login_box">
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+        </div>
     </div>
 </template>
 <script >
@@ -9,19 +13,50 @@ export default {
 
     // data() {
     //     return {
-            
+
     //     };
     // },
 
     // mounted() {
-        
+
     // },
 
     // methods: {
-        
+
     // },
 };
 </script >
 <style lang="less" scoped>
-    
+.login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+}
+
+.login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    .avatar_box{
+        height: 130px;
+        width: 130px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        background-color: #fff;
+        img{
+            height: 100%;
+            widows: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
 </style>
