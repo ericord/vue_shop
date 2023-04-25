2fe73e0379a3617b8090d60979897ec20b304e17
chaonianye <chaonianye@cnpc.com.cn>
2023-04-25 14:36:03 +0800

    表单预验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -13,11 +13,11 @@
                 </el-form-item>
                 <!-- 密码 -->
                 <el-form-item prop="password">
-                    <el-input prefix-icon="el-icon-lock" v-model="loginForm.password" show-password ></el-input>
+                    <el-input prefix-icon="el-icon-lock" v-model="loginForm.password" show-password></el-input>
                 </el-form-item>
                 <!-- 按钮区域 -->
                 <el-form-item class="btns">
-                    <el-button type="primary">登录</el-button>
+                    <el-button type="primary" @click="login">登录</el-button>
                     <el-button type="info" @click="restLoginForm">重置</el-button>
                 </el-form-item>
             </el-form>
@@ -56,8 +56,13 @@ export default {
     },
 
     methods: {
-        restLoginForm(){
+        restLoginForm() {
             this.$refs.loginFormRef.resetFields();
+        },
+        login() {
+            this.$refs.loginFormRef.validate(valid => {
+                console.log(valid)
+            });
         }
     },
 };
