e9931ffd138803586c5a7ec51147f5166a358147
chaonianye <chaonianye@cnpc.com.cn>
2023-04-26 12:06:19 +0800

    优化按需组件导入形式

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Form, FormItem } from 'element-ui'
-import { Input } from 'element-ui'
-import { Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
-Vue.use(Button)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(Input)
+const elements = [Button, Form, FormItem, Input]
+elements.forEach((e) => Vue.use(e))
 Vue.prototype.$message = Message
