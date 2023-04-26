149788307bc122aae7f66c6eaf9b437123525ebf
chaonianye <chaonianye@cnpc.com.cn>
2023-04-26 13:59:05 +0800

    主页布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header
+      >Header<el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+      <!-- 页面主体区域 -->
+      <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px">Aside</el-aside>
+        <!-- 右侧内容主题 -->
+        <el-main>Main</el-main>
+      </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -21,4 +31,17 @@ export default {
   },
 }
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #363d40;
+}
+.el-aside {
+  background-color: #313743;
+}
+.el-main {
+  background-color: #e9edf1;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+} from 'element-ui'
 
-const elements = [Button, Form, FormItem, Input]
+const elements = [Button, Form, FormItem, Input, Container, Header, Aside, Main]
 elements.forEach((e) => Vue.use(e))
 Vue.prototype.$message = Message
