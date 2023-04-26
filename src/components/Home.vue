315761e13e7de37bf9f84867518f45bc4cac2a24
chaonianye <chaonianye@cnpc.com.cn>
2023-04-26 14:33:11 +0800

    首页布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <el-container class="home-container">
     <!-- 头部区域 -->
-    <el-header
-      >Header<el-button type="info" @click="logout">退出</el-button>
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
     </el-header>
-      <!-- 页面主体区域 -->
-      <el-container>
-        <!-- 侧边栏 -->
-        <el-aside width="200px">Aside</el-aside>
-        <!-- 右侧内容主题 -->
-        <el-main>Main</el-main>
-      </el-container>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主题 -->
+      <el-main>Main</el-main>
+    </el-container>
   </el-container>
 </template>
 <script>
@@ -36,7 +40,23 @@ export default {
   height: 100%;
 }
 .el-header {
-  background-color: #363d40;
+  background-color: #2f3637;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+  img {
+    height: 60px;
+  }
 }
 .el-aside {
   background-color: #313743;
