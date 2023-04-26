3c043bc70e1783ce484afd8380d5812f8fd9c5b3
chaonianye <chaonianye@cnpc.com.cn>
2023-04-26 08:45:02 +0800

    退出功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        Homeeeeeesss
+        <el-button type="info" @click="logout">退出</el-button>
     </div>
 </template>
 <script>
@@ -9,19 +9,19 @@ export default {
 
     data() {
         return {
-            
+
         };
     },
 
     mounted() {
-        
     },
-
+    
     methods: {
-        
+        logout(){
+            window.sessionStorage.clear();
+            this.$router.push('/login');
+        }
     },
 };
 </script>
-<style lang="less" scoped>
-    
-</style>
+<style lang="less" scoped></style>
