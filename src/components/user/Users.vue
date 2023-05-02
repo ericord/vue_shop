42ac4ebd7a7ada0a427f0221da41ecc596b1fea7
chaonianye <chaonianye@cnpc.com.cn>
2023-05-02 21:09:09 +0800

    用户列表数据的获取

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -7,32 +7,55 @@
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
-    <el-card> 
-        <el-row gutter="20">
-            <el-col :span="8">
-                <el-input placeholder="请输入内容">
-                    <el-button slot="append" icon="el-icon-search"></el-button>
-                </el-input>
-            </el-col>
-            <el-col :span="4">
-                <el-button type="primary">添加用户</el-button>
-            </el-col>
-        </el-row>
-
-    </input> </el-card>
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 <script>
 export default {
   name: 'VueShopUsers',
+  created() {
+    this.getUserList()
+  },
 
   data() {
-    return {}
+    return {
+      userList: [],
+      totol: 0,
+      queryInfo: {
+        query: '',
+        pageNum: 1,
+        pageSize: 10,
+      },
+    }
   },
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get('/users', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200)
+        return this.$message.error('获取用户列表失败')
+      this.$message.success("获取用户列表成功")
+      this.userList = res.data.users
+      this.totol = res.data.total
+      this.pageNum = res.data.pageNum
+      console.log(res.data)
+    },
+  },
 }
 </script>
 <style lang="less" scoped></style>
