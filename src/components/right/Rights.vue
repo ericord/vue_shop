a4628d2cd71d34de1331ca7f4c1f0a509e45ed24
chaonianye <chaonianye@cnpc.com.cn>
2023-05-06 13:44:43 +0800

    权限管理页面路由

---
+++ src/components/right/Rights.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <!-- 面包屑区域 -->
+        <!-- 卡片区域 -->
+    </div>
+</template>
+<script>
+export default {
+    name: 'VueShopRights',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+<style lang="less" scoped>
+    
+</style>
