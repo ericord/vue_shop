a69a0bc97c16b3037b1bc22da887f2bbc7fc6bce
chaonianye <chaonianye@cnpc.com.cn>
2023-05-25 21:43:09 +0800

    roles页面路由

---
+++ src/components/power/Roles.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Roles</div>
+</template>
+<script>
+export default {
+  name: 'VueShopRoles',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+<style lang="less" scoped></style>
